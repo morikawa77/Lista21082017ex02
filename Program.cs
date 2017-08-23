4998e49d4d7ff168644c0fd101a39384c2622237
Reginaldo Morikawa <reginaldo@wolfandpartners.com.br>
2017-08-23 07:36:21 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lista21082017ex02
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			int totalEleitores, votosBrancos, votosNulos, votosValidos;
+            decimal porcBrancos, porcNulos, porcValidos;
+			Console.WriteLine("Digite a quantidade de eleitores: ");
+            totalEleitores = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade de votos brancos: ");
+            votosBrancos = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade de votos nulos: ");
+            votosNulos = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade de votos válidos: ");
+            votosValidos = Convert.ToInt32(Console.ReadLine());
+
+            if ((votosBrancos + votosNulos + votosValidos) == totalEleitores)
+            {
+                porcBrancos = (votosBrancos * 100) / totalEleitores;
+                porcNulos = (votosNulos * 100) / totalEleitores;
+                porcValidos = (votosValidos * 100) / totalEleitores;
+                Console.WriteLine("Porcentagem de votos brancos: {0}%", porcBrancos);
+                Console.WriteLine("Porcentagem de votos nulos: {0}%", porcNulos);
+                Console.WriteLine("Porcentagem de votos válidos: {0}%", porcValidos);
+                Console.ReadLine();
+            } else {
+				Console.WriteLine("Quantidade de votos inválida.");
+				Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Lista21082017ex01 C# Curso Programação de Dispositivos Móveis SENAC
